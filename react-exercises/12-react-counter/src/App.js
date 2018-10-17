7d515da656d89c7c8c99cab81fad28a2b45ada6d
Simi Asaro <sasaro@advancelocal.com>
2018-10-17 18:03:20 -0400

    12-react-counter react exercise

--- react-exercises/12-react-counter/src/App.js
+++ react-exercises/12-react-counter/src/App.js
@@ -5,6 +5,8 @@
  * As a user, I should not be able to go below 0 on the count
  * if the count goes above 10, the count should not be rendered to the sreen
  */
+import React from 'react'
+import './index.css'
 
 class App extends React.Component {
   constructor(props) {
@@ -13,14 +15,25 @@ class App extends React.Component {
       count: 0
     };
   }
+  counterIncrement = () => {
+    this.setState((state) => ({ count: state.count + 1 }))
+  }
+  counterDecrement = () => {
+    this.setState((state) => ({ count: state.count - 1 }))
+  }
+  counterReset = () => {
+    this.setState(() => ({ count: 0 }));
+  }
   render() {
     return (
       <div>
-        <button className="inc">Increment!</button>
-        <button className="dec">Decrement!</button>
-        <button className="reset">Reset</button>
+        <button className="inc" onClick={() => {this.counterIncrement()}}>Increment!</button>
+        <button className="dec" onClick={() => { this.counterDecrement()}}>Decrement!</button>
+        <button className="reset" onClick={() => { this.counterReset() }}>Reset</button>
         <h1>Current Count: {this.state.count}</h1>
       </div>
     );
   }
 }
+
+export default App;
