ab8b810ae346f9756850b16875a3c2646a8fb7a5
Matina Patsos <mpatsos@deepbluecommunications.com>
2018-10-30 19:57:59 -0400

    Redux assignment

---
+++ react-exercises/16-redux/src/actions.js
@@ -0,0 +1,14 @@
+/**
+ * Import action types from "src/constants/actionTypes.js"
+ */
+
+/**
+ * Create and export an a function for each action
+ * @example
+ * export function addTodo(userInput) => {
+ *   return {
+ *     type: "ADD_TODO",
+ *     userInput: userInput
+ *   }
+ * };
+ */
