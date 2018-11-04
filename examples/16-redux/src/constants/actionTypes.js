a694d17e78c49be62d258db7420613c33e3df0db
Matina Patsos <mpatsos@deepbluecommunications.com>
2018-11-04 17:05:37 -0500

    Simple REDUX example

---
+++ examples/16-redux/src/constants/actionTypes.js
@@ -0,0 +1,5 @@
+export const INCREMENT = "INCREMENT";
+export const DECREMENT = "DECREMENT";
+export const ADD_TODO = "ADD_TODO";
+export const DELETE_TODO = "DELETE_TODO";
+export const SELECT_COLOR = "SELECT_COLOR";
---
+++ examples/16-redux/test/reducer-test.js
@@ -0,0 +1,90 @@
+import { expect } from "chai";
+import * as type from "../src/constants/actionTypes";
+import reducer from "../src/reducer";
+
+describe("./src/reducer.js", () => {
+  it("should increase the count by 1", () => {
+    const state = {
+      count: 0
+    };
+    const action = {
+      type: type.INCREMENT
+    };
+    const newState = reducer(state, action);
+    expect(newState).to.deep.equal({
+      count: 1
+    });
+  });
+  it("should descrease the count by 1", () => {
+    const state = {
+      count: 5
+    };
+    const action = {
+      type: type.DECREMENT
+    };
+    const newState = reducer(state, action);
+    expect(newState).to.deep.equal({
+      count: 4
+    });
+  });
+  it("should add user input to the TODO list", () => {
+    const state = {
+      count: 5,
+      todos: []
+    };
+    const action = {
+      type: type.ADD_TODO,
+      userInput: "Talk about Redux"
+    };
+    const newState = reducer(state, action);
+    expect(newState).to.deep.equal({
+      count: 5,
+      todos: ["Talk about Redux"]
+    });
+  });
+  it("should remove an item from the TODO list", () => {
+    const state = {
+      count: 5,
+      todos: ["Buy Halloween custome", "Finish first project", "Get candy"]
+    };
+    const action = {
+      type: type.DELETE_TODO,
+      todoIndex: 1
+    };
+    const newState = reducer(state, action);
+    expect(newState).to.deep.equal({
+      count: 5,
+      todos: ["Buy Halloween custome", "Get candy"]
+    });
+  });
+  it("should select a color from a color list", () => {
+    const state = {
+      count: 5,
+      colors: {
+        1: "red",
+        2: "orange",
+        3: "yellow",
+        4: "green",
+        5: "blue",
+        6: "purple"
+      }
+    };
+    const action = {
+      type: type.SELECT_COLOR,
+      colorId: 4
+    };
+    const newState = reducer(state, action);
+    expect(newState).to.deep.equal({
+      count: 5,
+      colors: {
+        1: "red",
+        2: "orange",
+        3: "yellow",
+        4: "green",
+        5: "blue",
+        6: "purple"
+      },
+      selectedColor: "green"
+    });
+  });
+});
